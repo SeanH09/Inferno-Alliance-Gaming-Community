71a1d3285b590899558c9952eefe3a792e33a7a8
SeanH09 <seanheemeyer@gmail.com>
2019-09-16 01:01:24 +1200

    Moved from gatsby to standalone reactjs
    Moved from gatsby to standalone reactjs

--- inferno-alliance/src/componets/Navbar.js
+++ inferno-alliance/src/componets/Navbar.js
@@ -1,10 +1,21 @@
 import React from 'react';
 
-const navbar = () => <div class="navbar"> 
+const navbar = () => <nav class="navbar navbar-dark bg-dark"> 
     <ul>
         <li> <img src={require('../flame.png')}  width="40" height="40" alt="logo inferno"/> </li>
         <li> <p> Inferno Alliance </p></li>
+        <li> <p> 
+            <div class="line"> </div>
+            <div class="line"> </div>
+            <div class="line"> </div>
+        </p> </li>
     </ul>
-</div>
+
+    <ul id="NavItems">
+        <li> <p> Games </p> </li>
+        <li> <p> Community </p> </li>
+        <li> <p> Contact Us </p></li>
+    </ul>
+</nav>
 
 export default navbar;
